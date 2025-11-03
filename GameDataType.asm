c8a4ac52773bbbb0c0e1e2bc6acf8ed969fed8aa
PENG CONG-WEI <pengc5870@gmail.com>
2025-11-03 18:08:25 +0800

    implement TOOLSLOT related function.
    ShowToolSlot and SetToolSlot, BpPositiontoRPosition is Experimental and not yet being used.

--- GameDataType.asm
+++ GameDataType.asm
@@ -2,28 +2,60 @@ INCLUDE Irvine32.inc
 INCLUDE graph.inc
 
 .data
-	FrameWidth  WORD 8
-	FrameHight WORD 8
+	FrameWidth   WORD 8
+	FrameHight   WORD 8
+	PicWidth     WORD 6
+	PicHight     WORD 6
+	FrameColor   WORD 07h
+	FrameElement BYTE '*'
+	BackPackPosition COORD<0,0>  ;背包最左上角的位置
 .code
 
-SetToolSlot proc uses esi eax ecx Object: PTR TOOLSLOT, Pic: PTR BYTE
+BpPositiontoRPosition proc uses esi ebx ecx BpPosition: COORD
+
+	lea esi, BpPosition
+	mov bx, [esi]
+	movzx eax,  FrameWidth
+	mul bx
+	shr eax, 16
+	push eax
+	xor eax, eax
+	mov bx, [esi+2]
+	movzx eax,  FrameHight
+	mul bx
+	pop ebx
+	add eax, ebx
+	ret 4
+
+BpPositiontoRPosition endp
+
+
+SetToolSlot proc uses esi eax ecx edi Object: PTR TOOLSLOT, Source: PTR BYTE, Color: WORD
 	
 	mov esi, Object
-	mov eax, Pic
-	mov [esi+11], eax
-	mov [esi], '*'
-	mov [esi+1], 07h
-	mov [esi+3], FrameWidth
-	mov [esi+5], FrameHight
-	mov [esi+]	
- 
-	ret 8
+	INVOKE SetRectangle, esi, FrameElement, FrameColor, FrameWidth, FrameHight, BackPackPosition
+	add esi, 11
+	INVOKE SetPicture, esi, Source, Color, PicWidth, PicHight, BackPackPosition  
+	ret 12
 
 SetToolSlot endp
 
-ShowToolSlot proc object: PTR TOOLSLOT
+ShowToolSlot proc uses eax esi ecx edi Object: PTR TOOLSLOT, Position: COORD
 
-	ret 4
+	mov esi, Object
+	lea edi, Position
+	mov eax, [edi]
+	mov [esi+7], eax
+	add WORD PTR [edi], 1
+	add WORD PTR [edi+2], 1
+	mov eax, [edi]
+	mov [esi+21], eax
+
+	INVOKE ShowRectangle, esi
+	add esi, 11
+	INVOKE ShowPicture, esi
+		
+	ret 8
 
 ShowToolSlot endp
 
--- main.asm
+++ main.asm
@@ -8,15 +8,19 @@ main EQU start@0
 ExitProcess proto,dwExitCode:dword
 
 .data
-	teststr1 BYTE "*     *",
-		      "       ",
-		      "  ***  "
+	teststr1 BYTE "  **  ",
+		      " *  * ",
+		      " *  * ",
+		      "  **  ",
+		      " **** ",
+		      "  **  "
 	teststr2 BYTE "game start", 0Dh, 0Ah
 	rec1 RECTANGLE <>
 	P1 PICTURE <>
 	_title TEXT <>
 	color WORD 0Ah
-	Cursor COORD <3,3>
+	Tool1 TOOLSLOT <>
+	Cursor COORD <5,10>
 .code
 
 main proc
@@ -24,18 +28,26 @@ main proc
 	INVOKE Display_Init
 	INVOKE SetText, OFFSET _title, OFFSET teststr2, 0Ah, Cursor, LENGTHOF teststr2
 	INVOKE SetRectangle, OFFSET rec1, '*', 07h, 5, 3, Cursor
-	INVOKE SetPicture, OFFSET P1, OFFSET teststr1, 07h, 7, 3, Cursor
+	INVOKE SetPicture, OFFSET P1, OFFSET teststr1, 07h, 6, 6, Cursor
+	INVOKE SetToolSlot, OFFSET Tool1, OFFSET teststr1, 0Ah
 	
+	INVOKE ShowToolSlot, OFFSET Tool1, Cursor
+
 	;INVOKE ShowPicture, OFFSET P1
 	;INVOKE ErasePicture, OFFSET P1
 
-	INVOKE ShowRectangle, OFFSET rec1
+	;INVOKE ShowRectangle, OFFSET rec1
 	;INVOKE EraseRectangle, OFFSET rec1
 
 	;INVOKE ShowText, OFFSET _title
 	;INVOKE EraseText, OFFSET _title
-	call WaitMsg	
 
+	L1:
+		mov ecx, 0
+	loop L1
+		
+	call WaitMsg	
+	
 	invoke ExitProcess,0
 
 main endp

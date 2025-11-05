829fa62227a2af896343f2b613a5b1d48d25b142
Illusionary-Night <0s5h1a2s@gmail.com>
2025-11-05 21:54:42 +0800

    Add tool management procedures and update TOOL struct
    Implemented MemClone, SetProtoTool, ExecuteTool, and CooldownUpdate_Tool procedures in ToolDataType.asm for managing tool data and cooldown logic. Updated TOOL struct in ToolDataType.inc to include SHAPE, COOLDOWNMAX, TYPEID, and UUID fields, and revised related prototypes for improved tool handling.

--- ToolDataType.asm
+++ ToolDataType.asm
@@ -1,9 +1,70 @@
 INCLUDE Irvine32.inc
 INCLUDE graph.inc
-
-
+INCLUDE GameDataType.inc
+INCLUDE ToolDataType.inc
 .data
 	
 .code
+MemClone PROC USES esi edi ecx, Object:PTR BYTE, Source:PTR BYTE, _Length:DWORD
+    mov esi, Source
+    mov edi, Object
+    mov ecx, _Length
+    rep movsb
+    ret
+MemClone ENDP
 
-end
+SetProtoTool PROC USES esi edi eax,     ;用於設置道具種類，而非新增
+	Object: PTR TOOL,
+	Slot: PTR TOOLSLOT,	; 陣列起始座標，長度16 TOOLSLOT
+	Shape: PTR BYTE,	; 陣列起始座標，長度16 BYTE
+	Rarity: BYTE,
+	CooldownMax: DWORD,
+	TypeID: DWORD,
+
+    mov esi, Object
+    
+    lea edi, (TOOL PTR [esi]).SLOT
+    INVOKE MemClone, edi, Slot, SIZEOF TOOLSLOT * 16
+    
+    lea edi, (TOOL PTR [esi]).SHAPE
+    INVOKE MemClone, edi, Shape, SIZEOF BYTE * 16
+    
+    mov al, Rarity
+    mov (TOOL PTR [esi]).RARITY, al
+    
+    mov eax, CooldownMax
+    mov (TOOL PTR [esi]).COOLDOWNMAX, eax
+    
+    mov eax, TypeID
+    mov (TOOL PTR [esi]).TYPEID, eax
+    
+    ret
+SetProtoTool ENDP
+
+ExecuteTool PROC USES esi,
+	Object: PTR TOOL
+
+    mov esi, Object
+    ; 將角色delta屬性加到角色身上
+    ret
+ExecuteTool ENDP
+
+CooldownUpdate_Tool PROC USES esi eax,
+	Object: PTR TOOL
+
+    mov esi, Object
+    mov eax, (TOOL PTR [esi]).COOLDOWN
+    add eax, 1
+    cmp eax, (TOOL PTR [esi]).COOLDOWNMAX
+    jb Label_end
+    mov eax, 0
+    INVOKE ExecuteTool, Object
+
+Label_end:
+    mov (TOOL PTR [esi]).COOLDOWN, eax
+    ret
+CooldownUpdate_Tool ENDP
+
+
+
+END

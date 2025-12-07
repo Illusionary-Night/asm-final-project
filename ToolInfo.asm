a30d0111063f724b645344ba093a84baa3807c3a
Illusionary-Night <0s5h1a2s@gmail.com>
2025-12-07 16:35:34 +0800

    fix bug

--- ToolInfo.asm
+++ ToolInfo.asm
@@ -138,7 +138,7 @@ INCLUDE ./asm-final-project/MemOperation.inc
 	fire_cooldown_max DWORD 4
 	fire_typeid DWORD 1
 	fire_ally_delta INGAMEATTRIBUTE  <+100,+10,+10>
-	fire_enemy_delta INGAMEATTRIBUTE  <-100,-0,0>
+	fire_enemy_delta INGAMEATTRIBUTE  <-100,0,0>
 	fire_name BYTE "Soul Flame",0, 0,0,0,0,0,0,0,0,0,0,0  ; 總共 20 bytes
 	fire_price DWORD 5
 	;--------------------------------------------------------------------
@@ -361,14 +361,14 @@ INCLUDE ./asm-final-project/MemOperation.inc
 	arcane_blade_tool_slot TOOLSLOT 16 DUP(<>)
 	arcane_blade_tool_shape BYTE	"1000",
 									"1000",
-									"1000",
-									"1000"
+									"0000",
+									"0000"
 	arcane_blade_rarity BYTE 5
 	arcane_blade_cooldown_max DWORD 3
 	arcane_blade_typeid DWORD 1
 	arcane_blade_ally_delta INGAMEATTRIBUTE  <0,-20,0>
 	arcane_blade_enemy_delta INGAMEATTRIBUTE  <-100,0,0>
-	arcane_blade_name BYTE "Arcane Blade",0
+	arcane_blade_name BYTE "Arcane Blade",0,0,0,0,0,0,0,0 ; 總共 20 bytes
 	arcane_blade_price DWORD 20
 	;--------------------------------------------------------------------
 ; the_grace_cross tool data---------------------------------------------------
